7faff2a6bc61673d13f1d04bdd29042d3f99ff0a
Truongdinh150395 <Truongdinh150395@gmail.com>
2020-07-30 21:34:59 +0700

    file main

---
+++ main.js
@@ -0,0 +1,22 @@
+let sinhvien = new Sinhvien("truong");
+let sinhvienManager  = new SinhvienManager();
+
+
+        function showAll() {
+        document.getElementById("tbl-sinhvien ").innerHTML = sinhvienManager.getAllSinhvien();
+        }
+        function  getElementValue(id) {
+            return document.getElementById(id + '-sinhvien').value;
+        }
+      function addSinhVien(){
+        let id = getElementValue("id");
+        let name = getElementValue("name");
+        let date = getElementValue("date");
+        let lop = getElementValue("lop");
+        let diem= getElementValue("diem");
+        let img = getElementValue("img");
+
+        let sinhvien = new Sinhvien(id,name,date,lop,diem,img);
+        sinhvienManager.addSinhvien(sinhvien);
+        showAll();
+    }
